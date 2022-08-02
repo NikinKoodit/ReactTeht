1575f430d768a3223434ba25e0f4e6d34cd05185
NikinKoodit <niki.miettinen@awacademy.training>
2022-08-02 11:11:49 +0300

    Tehtävä 1.1 tehtynä

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course = {course}/>
+      <Content part = {part1} exercices= {exercises1}/>
+      <Content part = {part2} exercices= {exercises2}/>
+      <Content part = {part3} exercices= {exercises3}/>
+      
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const Header = (props) => {
+
+return(
+  <div> 
+    <h1>{props.course}</h1>
+  </div>
+
+)
+}
+
+const Content = (props) =>{
+return(
+  <p>
+    {props.part} {props.exercices}
+  </p>
+)
+
+}
+
+export default App
