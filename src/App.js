30c87841c94ae015a86950f8fceb3aaf43ba4706
NikinKoodit <niki.miettinen@awacademy.training>
2022-08-02 15:12:57 +0300

    teht 1.4 tehty

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-//tehtävä 1.1
+//tehtävä 1.4
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course = {course}/>
-      <Content part = {part1.name} exercices= {part1.exercises}/>
-      <Content part = {part2.name} exercices= {part2.exercises}/>
-      <Content part = {part3.name} exercices= {part3.exercises}/>
-      <Total exercicesAll = {part1.exercises + part2.exercises + part3.exercises} />
+      <Content part = {parts[0].name} exercices={parts[0].exercises}/>
+      <Content part = {parts[1].name} exercices={parts[1].exercises}/>
+      <Content part = {parts[2].name} exercices={parts[2].exercises}/>
+      <Total exercicesAll = {parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
@@ -37,11 +39,10 @@ return(
 }
 
 const Content = (props) =>{
-  console.log(props)
+  console.log(props)  
 return(
   <p>
-    {props.part} {props.exercices},
-    
+    {props.part} {props.exercices}
   </p>
 )
 }
